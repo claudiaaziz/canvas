5aa4e6cee1c5b0542e1c094024d57b7b673ed489
claudiaaziz <claudiahannahh@gmail.com>
2023-10-20 23:01:53 -0400

    Revert back to 4caec93

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import Canvas from "./scripts/canvas";
-import DrawingHandler from "./scripts/drawing_handler";
 import Brush from "./scripts/brush";
-import ColorHandler from "./scripts/color_handler";
-import UndoHandler from "./scripts/undo_handler";
 
 document.addEventListener("DOMContentLoaded", () => {
   new Canvas();
