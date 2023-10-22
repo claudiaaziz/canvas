8ad57a63d2cf19e40baadfb53db0e64d94637e3d
claudiaaziz <claudiahannahh@gmail.com>
2023-10-21 20:24:11 -0400

    Revert back to 50c2cf0

--- src/index.js
+++ src/index.js
@@ -5,31 +5,4 @@ document.addEventListener("DOMContentLoaded", () => {
   new Canvas();
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const colorPickerInput = document.getElementById("color-picker");
 
-  // Initialize pickr Color Picker and show immediately
-  const pickr = Pickr.create({
-    el: colorPickerInput,
-    theme: "classic",
-    default: "#ECD1E2", // No default color, let users choose from the color wheel
-    components: {
-      preview: true,
-      opacity: true,
-      hue: true,
-      interaction: {
-        hex: true,
-        rgba: true,
-        hsla: true,
-        hsva: true,
-        cmyk: true,
-        input: true,
-        clear: true,
-        save: true,
-      },
-    },
-  });
-
-  // Show the color picker immediately
-  pickr.show();
-});
