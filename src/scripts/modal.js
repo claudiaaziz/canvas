5f9172f948945d8c62c5e35c67f0fb69013b1c57
claudiaaziz <claudiahannahh@gmail.com>
2023-12-03 14:35:24 -0500

    Add global click event listener to close the modal if clicked outside of it

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,6 +1,6 @@
 class Modal {
   constructor() {
-    this.setUpModel()
+    this.setUpModel();
     this.showModal();
   }
 
@@ -8,20 +8,22 @@ class Modal {
     this.modal = document.getElementById("modal");
     this.closeButtons = document.querySelectorAll(".close-button");
     this.helpIcon = document.getElementById("help-icon");
-    
+
     this.closeButtons.forEach((button) =>
       button.addEventListener("click", () => this.closeModal())
     );
     this.helpIcon.addEventListener("click", () => this.showModal());
+
+    document.addEventListener("click", (event) => {
+      if (event.target === this.modal) {
+        this.closeModal();
+      }
+    });
   }
 
-  showModal = () => {
-    this.modal.style.display = "flex";
-  };
+  showModal = () => (this.modal.style.display = "flex");
 
-  closeModal = () => {
-    this.modal.style.display = "none";
-  };
+  closeModal = () => (this.modal.style.display = "none");
 }
 
 export default Modal;
